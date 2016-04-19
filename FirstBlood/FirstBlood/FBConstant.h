7ee3cbc9c9177db12e32ed7202c2f21fb5da13e0
linjiaqiao <649207624@qq.com>
2016-04-19 16:35:42 +0800

    33
    33

--- FirstBlood/FirstBlood/FBConstant.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-UIKIT_EXTERN const CGFloat titleRed;
-UIKIT_EXTERN const CGFloat titleGreen;
-UIKIT_EXTERN const CGFloat titleBlue;
-UIKIT_EXTERN NSString * const XMGName;
--- FirstBlood/FirstBlood/FBConstant.m
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-const CGFloat titleRed = 0.4;
-const CGFloat titleGreen = 0.6;
-const CGFloat titleBlue = 0.7;
-NSString * const XMGName = @"jack";
